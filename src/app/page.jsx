9a57fb4bccd587e28263b0af0df856f029a1cec1
GkkyShab <gouravgupta.0300@gmail.com>
2024-06-17 20:43:04 +0530

    Updated Resume and Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -21,29 +21,27 @@ const Homepage = () => {
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
-            Crafting Digital Experience, Designing Tommorow.
+            Crafting the Backbone of Digital Solutions.
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-            Welcome to my digital canvas, Where innovation and creativity
-            coverge. With the keen eye for the aesthetics and a amstery of code,
-            my portfolio showcases a diverse collection of projects that
-            reflects my commitment to excellence.
+            Welcome to my portfolio, where precision meets innovation. As a
+            backend developer, I showcase projects that highlight my expertise
+            in creating scalable and efficient systems. Explore my work and see
+            how I turn complex challenges into seamless digital experiences.
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
-            <Link 
-            href="/portfolio"
-            className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+            <Link
+              href="/portfolio"
+              className="p-4 rounded-lg ring-1 ring-black bg-black text-white"
+            >
               View My Work
             </Link>
-            
-            <Link
-            href="/contact"
-            className="p-4 rounded-lg ring-1 ring-black"
-          >
-            Contact Me
-          </Link>
+
+            <Link href="/contact" className="p-4 rounded-lg ring-1 ring-black">
+              Contact Me
+            </Link>
           </div>
         </div>
       </div>
